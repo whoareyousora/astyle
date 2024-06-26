56f0026dd95e857ef10ed2deb245cad84d75568d
whoareyousora <whyareyousora@gmail.com>
2024-06-26 15:09:13 +0300

    files

---
+++ file.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 7;
+    
+    int sum = num1 + num2;
+    
+    std::cout << "Сумма чисел " << num1 << " и " << num2 << " равна " << sum << std::endl;
+    
+    return 0;
+}
+
